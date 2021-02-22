2a0f831c6e6fddfa0f0f7e4f9e9a776564764256
Sreecharanya Srinivasan <sree3charanya@gmail.com>
2021-02-22 02:29:34 +0000

    chapter7part2(1)

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ let dna = " TCG-TAC-gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
 
 // First, print out the dna strand in it's current state.
 
-
+console.log(dna)
+console.log("----------------------------------")
 //a) Use the .trim() method to remove the leading and trailing whitespace, then print the result.
-
-console.log(/* Your code here. */);
+dna=dna.trim();
+console.log("Trim",dna);
+console.log("----------------------------------")
 
 //b) Change all of the letters in the dna string to UPPERCASE, then print the result.
-
-console.log();
+dna=dna.toUpperCase()
+console.log("To Uppercase",dna);
+console.log("----------------------------------")
 
 //c) Note that after applying the methods above, the original, flawed string is still stored in dna. To fix this, we need to reassign the changes to back to dna.
 //Apply these fixes to your code so that console.log(dna) prints the DNA strand in UPPERCASE with no whitespace.
